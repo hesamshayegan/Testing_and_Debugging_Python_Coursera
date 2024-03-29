71946415275436f8a93dee03281b5b5f43203f44
hesam <hesamshayegan88@gmail.com>
2024-03-29 13:58:14 -0700

    added bubblesort + test

---
+++ bubble_sort_test.py
@@ -0,0 +1,40 @@
+import unittest
+import bubblesort as st
+import random
+
+class Test_TestValue(unittest.TestCase):
+    def test_simple(self):
+        testList = [4,3,2,1]
+        expectedList = testList.copy()
+        expectedList.sort()
+        actualList = st.bubbleSort(testList)            
+        self.assertEqual(actualList, expectedList)
+
+    def test_empty(self):
+        testList = []
+        expectedList = testList.copy()
+        expectedList.sort()
+        actualList = st.bubbleSort(testList)
+        self.assertEqual(actualList, expectedList)
+
+    def test_presorted(self):
+        testList = [1,2,3,4]
+        expectedList = testList.copy()
+        expectedList.sort()
+        actualList = st.bubbleSort(testList)
+        self.assertEqual(actualList, expectedList)
+
+    
+    def test_negative(self):
+        testList = [-4,-3,-2,-1]
+        expectedList = testList.copy()
+        expectedList.sort()
+        actualList = st.bubbleSort(testList)
+        self.assertEqual(actualList, expectedList)
+
+    def test_random(self):
+        testList = [random.randint(0,20), random.randint(0,20), random.randint(0,20), random.randint(0,20)]
+        expectedList = testList.copy()
+        expectedList.sort()
+        actualList = st.bubbleSort(testList)
+        self.assertEqual(actualList, expectedList) 
